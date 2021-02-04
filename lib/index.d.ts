004fdee3f07e7f89cf202ffab7e4becc1a15a69b
nosvalds <niko@devinit.org>
2021-02-04 10:25:59 +0000

    return stdout and stderr in an object

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,29 +4,29 @@
  *
  * @param input XML
  */
-export declare const validateXML: (input: string | Buffer) => Promise<string | void>;
+export declare const validateXML: (input: string | Buffer) => Promise<string | void | Object>;
 /**
  * Validate XML without any DTD or schema. Return Recovered XML.
  *
  * @param input XML
  */
-export declare const validateXMLrecover: (input: string | Buffer, output: string | Buffer) => Promise<string | void>;
+export declare const validateXMLrecover: (input: string | Buffer) => Promise<string | void | Object>;
 /**
  * Validate XML without any DTD or schema. Output Recovered XML to file.
  *
  * @param input XML
  * @param output File path to store output
  */
-export declare const validateXMLrecoverOutput: (input: string | Buffer, output: string | Buffer) => Promise<string | void>;
+export declare const validateXMLrecoverOutput: (input: string | Buffer, output: string | Buffer) => Promise<string | void | Object>;
 /**
  * Validate XML with DTD.
  *
  * @param input XML
  */
-export declare const validateXMLWithDTD: (input: string | Buffer) => Promise<string | void>;
+export declare const validateXMLWithDTD: (input: string | Buffer) => Promise<string | void | Object>;
 /**
  * Validate XML with the provided XML schema file.
  * @param input XML
  * @param xsdfile Path to XSD
  */
-export declare const validateXMLWithXSD: (input: string | Buffer, xsdfile: string | Buffer) => Promise<string | void>;
+export declare const validateXMLWithXSD: (input: string | Buffer, xsdfile: string | Buffer) => Promise<string | void | Object>;
